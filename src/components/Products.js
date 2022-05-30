f9bc1f9ee4bdd9e930dfc5e466452b3469843efb
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-05-30 18:51:35 +0100

    Show each product

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { products as _products } from "./../data.js";
 import "./../style/card.css"
 
-const Products = () => {
+const Products = (props) => {
   return (
     <>
       <section className='section'>
@@ -11,24 +11,22 @@ const Products = () => {
           {
             _products.map((prd, i) => {
               return (
-                <>
-                  <li key={i}>
-                    <Link to={"/products/" + prd.id} className="card">
-                      <img src={prd.image} className="card__image" alt={prd.name} />
-                      <div className="card__overlay">
-                        <div className="card__header">
-                          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                          {/* <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" /> */}
-                          <div className="card__header-text">
-                            <h3 className="card__title">{prd.name}</h3>
-                            <span className="card__status">1 hour ago</span>
-                          </div>
+                <li key={i}>
+                  <Link to={"/products/" + prd.id} className="card">
+                    <img src={prd.image} className="card__image" alt={prd.name} />
+                    <div className="card__overlay">
+                      <div className="card__header">
+                        <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                        {/* <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" /> */}
+                        <div className="card__header-text">
+                          <h3 className="card__title">{prd.name}</h3>
+                          <span className="card__status" style={{ color: "var(--primary-500)" }}>See More <span>&rarr;</span></span>
                         </div>
-                        <p className="card__description"></p>
                       </div>
-                    </Link>
-                  </li>
-                </>
+                      <p className="card__description"></p>
+                    </div>
+                  </Link>
+                </li>
               )
             })
           }
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,7 +1,30 @@
-const SingleProduct = () => {
+import { products } from "./../data.js";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const SingleProduct = (props) => {
+  const { productId } = useParams();
+  const [product] = useState(products.find(p => p.id === productId));
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (product === undefined) {
+      return navigate('*');
+    }
+  }, [product, navigate])
+
+  useEffect(() => {
+    document.title = props?.title + ": " + product?.name + " 🛍"
+  })
+
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <Link to='/products' style={{ color: "var(--primary-500)" }}><span>&larr;</span> back to products</Link>
+      <div style={{ padding: "1.5rem" }}>
+        <h2>{product?.name}</h2>
+        <img src={product?.image} className="" alt={product?.name} />
+      </div>
     </section>
   );
 };
