e6492fcaa5af259542acf3a1904706d5c2de5de9
Oth-mane1 <othmane.ouirdy@gmail.com>
2022-05-30 18:50:58 +0100

    Implement images

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+const aboutImg = require("./../images/About us page-cuate.svg")
 
 const About = () => {
   return (
     <section className='section'>
-      <h2>About</h2>      
+      <h2>About</h2>
+      <img src={aboutImg.default} alt="about" />
+
       <Link to='/' className="btn">
         Go Back Home
       </Link>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
+const dashboardImg = require("./../images/Dashboard-cuate.svg")
 
 const Home = () => {
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <img src={dashboardImg.default} alt="Dashboard" />
+
       <Link to='/home/more' className="btn">
         Go To More
       </Link>
